bfd4d74cfb8d236cb763a7533972eaa8e30368b5
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-05-24 21:40:41 -0500

    new

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -4,3 +4,13 @@ console.log(tableData);
 
 var tbody = d3.select("tbody");
 console.log(data);
+
+data.forEach(function(ufoSighting){
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key,value]){
+        console.log(key,value);
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
