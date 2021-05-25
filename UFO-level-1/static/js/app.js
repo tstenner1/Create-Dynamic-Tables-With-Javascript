82cd29f7c88744531febd541777155e3f6308bac
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-05-24 21:45:24 -0500

    new

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,34 +1,34 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 console.log(tableData);
 
-var tbody = d3.select("tbody");
+let tbody = d3.select("tbody");
 console.log(data);
 
 data.forEach(function(ufoSighting){
     console.log(ufoSighting);
-    var row = tbody.append("tr");
+    let row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(function([key,value]){
         console.log(key,value);
-        var cell = tbody.append("td");
+        let cell = tbody.append("td");
         cell.text(value);
     });
 });
 
-var button = d3.select("#filter-btn");
+let button = d3.select("#filter-btn");
 
 button.on("click", function(event){
     d3.event.preventDefault();
     tbody.html("");
 
-var inputElement = d3.select("#datetime"); 
-var inputValue = inputElement.property("value");
+let inputElement = d3.select("#datetime"); 
+let inputValue = inputElement.property("value");
 
-var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+let filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
 filteredData.forEach(function(dateData){
-    var row=tbody.append("tr");
+    let row=tbody.append("tr");
     Object.entries(dateData).forEach(function([key,value]){
-    var cell=tbody.append("td");
+    let cell=tbody.append("td");
     cell.text(value);
         });
     });
