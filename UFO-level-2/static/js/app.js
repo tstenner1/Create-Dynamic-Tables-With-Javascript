f9f79813241ce3062909c863ab395ad2b8595421
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-05-24 21:43:31 -0500

    new

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,35 +1,34 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 console.log(tableData);
 
-var tbody = d3.select("tbody");
+let tbody = d3.select("tbody");
 console.log(data);
-
 
 data.forEach(function(ufoSighting){
     console.log(ufoSighting);
-    var row = tbody.append("tr");
+    let row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(function([key,value]){
         console.log(key,value);
-        var cell = tbody.append("td");
+        let cell = tbody.append("td");
         cell.text(value);
     });
 });
 
-var button = d3.select("#filter-btn");
+let button = d3.select("#filter-btn");
 
 button.on("click", function(event){
     d3.event.preventDefault();
     tbody.html("");
-    var inputElement = d3.select("#datetime"); 
-    var inputValue = inputElement.property("value");
-    var cityInput=d3.select("#city").property("value");
-    var stateInput=d3.select("#state").property("value");
-    var countryInput=d3.select("#country").property("value");
-    var shapeInput=d3.select("#shape").property("value");
+    let inputElement = d3.select("#datetime"); 
+    let inputValue = inputElement.property("value");
+    let cityInput=d3.select("#city").property("value");
+    let stateInput=d3.select("#state").property("value");
+    let countryInput=d3.select("#country").property("value");
+    let shapeInput=d3.select("#shape").property("value");
     console.log(cityInput);
 
-    var filterData=tableData;
+    let filterData=tableData;
     if (inputValue){
         filterData = filterData.filter(row => row.datetime === inputValue); 
     }
@@ -46,9 +45,9 @@ button.on("click", function(event){
         filterData = filterData.filter(row => row.shape === shapeInput);       
     }
     filterData.forEach(function(dateData){
-        var row=tbody.append("tr");
+        let row=tbody.append("tr");
         Object.entries(dateData).forEach(function([key,value]){
-        var cell=tbody.append("td");
+        let cell=tbody.append("td");
         cell.text(value);
             });
         });
